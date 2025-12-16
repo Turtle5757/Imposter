5b55ea0f9ab7acddc0d79cba7d715c714cfca24e
Turtle5757 <100431673+Turtle5757@users.noreply.github.com>
2025-12-16 07:46:02 -0500

    Delete words.js

--- words.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-{ category: 'Fruit', word: 'Apple' },
-{ category: 'Animal', word: 'Elephant' },
-{ category: 'Place', word: 'Beach' },
-{ category: 'Object', word: 'Chair' },
-{ category: 'Food', word: 'Pizza' }
-];
