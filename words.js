3ad66d220c0ede3188eae23105befbaf0d27a958
Turtle5757 <100431673+Turtle5757@users.noreply.github.com>
2025-12-16 07:18:28 -0500

    Create words.js

---
+++ words.js
@@ -0,0 +1,6 @@
+module.exports = [
+{ category: 'Fruits', word: 'Apple' },
+{ category: 'Animals', word: 'Elephant' },
+{ category: 'Places', word: 'Beach' },
+{ category: 'Objects', word: 'Chair' }
+];
